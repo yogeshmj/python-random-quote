be15da36c787b88218d7e3b8ea11853f994507de
Yogesh MJ <yogeshmj@nie.ac.in>
2020-02-23 23:47:25 +0530

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
-def primary:
-  print("Keep it logically awesome.")
+def main():
+
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes)
+  print(quotes[13])
 
 if __name__== "__main__":
-  primary()
+  main()
