e795f04a80e6783f19d5abf91ceeea7c270535ac
Yogesh MJ <yogeshmj@nie.ac.in>
2020-02-23 23:53:44 +0530

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
+import random
 def main():
 
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes[13])
+  last = 13
+  rnd = random.randint(0,last)
+  last = len(quotes) - 1
+  print(quotes[rnd])
 
 if __name__== "__main__":
   main()
